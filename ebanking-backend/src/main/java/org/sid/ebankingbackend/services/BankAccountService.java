dea0e267388d67ce9ee5c5f8708d84454c0f381b
AKASMIOU Ouassima <o.akasmiou@etu.enset-media.ac.ma>
2023-05-21 00:54:33 +0100

    VF

--- ebanking-backend/src/main/java/org/sid/ebankingbackend/services/BankAccountService.java
+++ ebanking-backend/src/main/java/org/sid/ebankingbackend/services/BankAccountService.java
@@ -36,4 +36,6 @@ public interface BankAccountService {
 
 
     AccountHistoryDTO getAccountHistory(String accountId, int page, int size) throws BankAccountNotFoundException;
+
+    List<CustomerDTO> searchCustomers(String keyword);
 }
